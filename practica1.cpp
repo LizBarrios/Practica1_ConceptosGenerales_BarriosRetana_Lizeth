a4f2768ec7e175e16fe397761167e7d9c339409a
LizBarrios <141792789+LizBarrios@users.noreply.github.com>
2023-08-16 22:14:43 -0700

    modified:   practica1.cpp 	modified:   practica1.exe

--- practica1.cpp
+++ practica1.cpp
@@ -1,14 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+float calculadora();
 int main ()
 {
-    float num1,num2;
+    int act;
+    float resultado;
     printf("ingrese el numero de la actividad que va a realizar");
     printf("1. Calculadora");
     printf("2. Juego de azar");
     printf("3. calculadora de areas");
+    scanf("%d",&act);
+    switch (act)
+    {
+    case 1: 
+        resultado=calculadora();
+    }
+    return 0;
+}
+float calculadora()
+{
+    float num1,num2,resul;
+    int opci;
     printf("Ingrese el primer numero:\n");
     scanf("%f",&num1);
     printf("Ingrese el segundo numero:\n");
     scanf("%f",&num2);
+    printf("ingrese el numero de la operacion que desea realizar?");
+    printf("1. sumar");
+    printf("2. restar");
+    printf("3. multiplicar");
+    printf("4. dividir");
+    scanf("%d",opci);
+    switch (opci)
+    {
+    case 1:
+        resul=num1+num2;
+        return resul;
+        break;
+    }
+
 }
