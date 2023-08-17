ffd47ac55ce2a2528f73d0ea04696e8ea7fa26ba
LizBarrios <141792789+LizBarrios@users.noreply.github.com>
2023-08-16 21:01:29 -0700

    Changes to be committed: 	modified:   practica1.cpp 	new file:   practica1.exe

---
+++ practica1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+    float num1,num2;
+    printf("ingrese el numero de la actividad que va a realizar");
+    printf("1. Calculadora");
+    printf("2. Juego de azar");
+    printf("3. calculadora de areas");
+    printf("Ingrese el primer numero:\n");
+    scanf("%f",&num1);
+    printf("Ingrese el segundo numero:\n");
+    scanf("%f",&num2);
+}
