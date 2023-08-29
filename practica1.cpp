ea148fa088bb77fbf407c6a93ede9692270215c2
LizBarrios <141792789+LizBarrios@users.noreply.github.com>
2023-08-28 23:24:35 -0700

    new file:   .vscode/tasks.json 	modified:   practica1.cpp 	deleted:    practica1.exe

--- practica1.cpp
+++ practica1.cpp
@@ -1,43 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-float calculadora();
+float calculadora(float num1, float num2);
 int main ()
 {
     int act;
-    float resultado;
-    printf("ingrese el numero de la actividad que va a realizar");
-    printf("1. Calculadora");
-    printf("2. Juego de azar");
-    printf("3. calculadora de areas");
+    float resultado,resul;
+    float num1,num2;
+    printf("ingrese el numero de la actividad que va a realizar:\n");
+    printf("1. Calculadora\n");
+    printf("2. Juego de azar\n");
+    printf("3. calculadora de areas\n");
     scanf("%d",&act);
     switch (act)
     {
     case 1: 
-        resultado=calculadora();
+        printf("Ingrese el primer numero:\n");
+        scanf("%f",&num1);
+        printf("Ingrese el segundo numero:\n");
+        scanf("%f",&num2);
+        resultado = calculadora(num1,num2);
+        printf("El resultado es: %.2f\n", resultado);
+        break;
     }
     return 0;
 }
-float calculadora()
+float calculadora(float resul)
 {
-    float num1,num2,resul;
+    float num1,num2;
     int opci;
-    printf("Ingrese el primer numero:\n");
-    scanf("%f",&num1);
-    printf("Ingrese el segundo numero:\n");
-    scanf("%f",&num2);
-    printf("ingrese el numero de la operacion que desea realizar?");
-    printf("1. sumar");
-    printf("2. restar");
-    printf("3. multiplicar");
-    printf("4. dividir");
-    scanf("%d",opci);
+    printf("ingrese el numero de la operacion que desea realizar?\n");
+    printf("1. sumar\n");
+    printf("2. restar\n");
+    printf("3. multiplicar\n");
+    printf("4. dividir\n");
+    scanf("%d",&opci);
     switch (opci)
     {
     case 1:
         resul=num1+num2;
-        return resul;
+        break;
+    case 2:
+        resul=num1-num2;
+        break;
+    case 3:
+        resul=num1*num2;
+        break;
+    case 4:
+        if (num2 != 0) {
+                resul = num1 / num2;
+            } else {
+                printf("No es posible dividir por cero.\n");
+            }
+        break;
+    default:
+        printf("no valido");
         break;
     }
-
+    return resul;
 }
